8f69393db4ccd2b80af2bbb8c587223b538338d9
Bikash Maity <104905898+DarkLord-99@users.noreply.github.com>
2022-06-06 08:51:51 +0530

    Update __main__.py

--- Rose/__main__.py
+++ Rose/__main__.py
@@ -105,7 +105,6 @@ home_keyboard_pm = InlineKeyboardMarkup(
             ),
         ] 
         ],
-    ]
 )
 
 keyboard = InlineKeyboardMarkup(
@@ -119,13 +118,8 @@ keyboard = InlineKeyboardMarkup(
     ]
 )
 
-IMG = ["https://telegra.ph/file/c8f5c1dd990ca9a3d8516.jpg",
-       "https://telegra.ph/file/77cc3154b752ce822fd52.jpg",
-       "https://telegra.ph/file/e72fb0b6a7fba177cf4c7.jpg",
-       "https://telegra.ph/file/8738a478904238e367939.jpg",
-       "https://telegra.ph/file/68d7830ba72820f44bda0.jpg"
-]
-
+IMG = ["https://telegra.ph/file/48fe6b5c77e7f240c00bf.jpg",
+  ]
 @app.on_message(filters.command(START_COMMAND))
 @language
 async def start(client, message: Message, _):
